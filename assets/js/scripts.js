b76da316c0ccbe342731582b94dec863e10080b7
kingsam91 <lambetgm@gmail.com>
2020-05-23 16:15:57 +0300

    adding hoover effect on portfolio

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,4 +9,43 @@ $(document).ready(function(){
    $(".p-icon").click(function() {
      $("#product-mgt-description").toggle();
    });
+   $("#work1").hover(function(){
+     $(this).css("background-color", "hotpink");
+    }, function(){
+     $(this).css("background-color", "white");
+   });
+   $("#work2").hover(function(){
+     $(this).css("background-color", "hotpink");
+    }, function(){
+    $(this).css("background-color", "white");
+   });
+   $("#work3").hover(function(){
+     $(this).css("background-color", "hotpink");
+   }, function(){
+   $(this).css("background-color", "white");
+   });
+   $("#work4").hover(function(){
+     $(this).css("background-color", "hotpink");
+   }, function(){
+   $(this).css("background-color", "white");
+   });
+   $("#work5").hover(function(){
+     $(this).css("background-color", "hotpink");
+   }, function(){
+   $(this).css("background-color", "white");
+   });
+   $("#work6").hover(function(){
+     $(this).css("background-color", "hotpink");}, function(){
+     $(this).css("background-color", "white");
+   });
+   $("#work7").hover(function(){
+     $(this).css("background-color", "hotpink");
+   }, function(){
+   $(this).css("background-color", "white");
+   });
+   $("#work8").hover(function(){
+     $(this).css("background-color", "hotpink");
+   }, function(){
+   $(this).css("background-color", "white");
+   });
 })
