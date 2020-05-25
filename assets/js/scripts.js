918e95923c59ca4cdd4b8f5890b4b8fdbf3a7def
kingsam91 <lambetgm@gmail.com>
2020-05-25 08:11:10 +0300

    add submit button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,51 +1,52 @@
-$(document).ready(function(){
-   $(".what-we-do").css("text-align", "center");
-   $(".d-icon").click(function() {
-     $("#design-description").toggle();
-   });
-   $(".dev-icon").click(function() {
-     $("#dev-description").toggle();
-   });
-   $(".p-icon").click(function() {
-     $("#product-mgt-description").toggle();
-   });
-   $("#work1").hover(function(){
-     $(this).css("background-color", "hotpink");
-    }, function(){
-     $(this).css("background-color", "white");
-   });
-   $("#work2").hover(function(){
-     $(this).css("background-color", "hotpink");
-    }, function(){
+$(document).ready(function() {
+  $(".what-we-do").css("text-align", "center");
+  $(".d-icon").click(function() {
+    $("#design-description").toggle();
+  });
+  $(".dev-icon").click(function() {
+    $("#dev-description").toggle();
+  });
+  $(".p-icon").click(function() {
+    $("#product-mgt-description").toggle();
+  });
+  $("#work1").hover(function() {
+    $(this).css("background-color", "hotpink");
+  }, function() {
     $(this).css("background-color", "white");
-   });
-   $("#work3").hover(function(){
-     $(this).css("background-color", "hotpink");
-   }, function(){
-   $(this).css("background-color", "white");
-   });
-   $("#work4").hover(function(){
-     $(this).css("background-color", "hotpink");
-   }, function(){
-   $(this).css("background-color", "white");
-   });
-   $("#work5").hover(function(){
-     $(this).css("background-color", "hotpink");
-   }, function(){
-   $(this).css("background-color", "white");
-   });
-   $("#work6").hover(function(){
-     $(this).css("background-color", "hotpink");}, function(){
-     $(this).css("background-color", "white");
-   });
-   $("#work7").hover(function(){
-     $(this).css("background-color", "hotpink");
-   }, function(){
-   $(this).css("background-color", "white");
-   });
-   $("#work8").hover(function(){
-     $(this).css("background-color", "hotpink");
-   }, function(){
-   $(this).css("background-color", "white");
-   });
+  });
+  $("#work2").hover(function() {
+    $(this).css("background-color", "hotpink");
+  }, function() {
+    $(this).css("background-color", "white");
+  });
+  $("#work3").hover(function() {
+    $(this).css("background-color", "hotpink");
+  }, function() {
+    $(this).css("background-color", "white");
+  });
+  $("#work4").hover(function() {
+    $(this).css("background-color", "hotpink");
+  }, function() {
+    $(this).css("background-color", "white");
+  });
+  $("#work5").hover(function() {
+    $(this).css("background-color", "hotpink");
+  }, function() {
+    $(this).css("background-color", "white");
+  });
+  $("#work6").hover(function() {
+    $(this).css("background-color", "hotpink");
+  }, function() {
+    $(this).css("background-color", "white");
+  });
+  $("#work7").hover(function() {
+    $(this).css("background-color", "hotpink");
+  }, function() {
+    $(this).css("background-color", "white");
+  });
+  $("#work8").hover(function() {
+    $(this).css("background-color", "hotpink");
+  }, function() {
+    $(this).css("background-color", "white");
+  });
 })
